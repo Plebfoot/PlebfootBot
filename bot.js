d917a3c36f37ce5896e165429aebcd5a23785ac3
Plebfoot <33572052+Plebfoot@users.noreply.github.com>
2017-11-12 15:30:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!ping'){
-     bot.sendMessage(message,'Pong!');
+    message.reply(message,'Pong!');
     }
 });
 
