411ba398a9b276daac3cbfc3163431334fbbd7fa
Plebfoot <33572052+Plebfoot@users.noreply.github.com>
2017-11-12 19:04:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,5 +16,14 @@ client.on('message', message => {
       message.channel.send('http://www.paypal.me/Tjony')
     }
 });
+
+  client.on('message', message => {
+    if (message.content === '!help'){
+      message.channel.send(''Dit zijn de commands!
+                             !help, Kom je hier uit. 
+                             !ping, Kan je kijken of de bot werkt.
+                             !geefgeld, Is gewoon een trol command.'')
+    }
+});
 // THIS MUST BE THIS WAY
 client.login(process.env.BOT_TOKEN);
